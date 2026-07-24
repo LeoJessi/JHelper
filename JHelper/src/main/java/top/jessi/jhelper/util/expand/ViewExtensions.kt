e434e542df2efdab08d1393cdc227397e1168db3
Jessi <17324719944@189.cn>
2026-07-24 19:48:39 +0800

    增加View设置宽高与外间距的拓展函数

--- JHelper/src/main/java/top/jessi/jhelper/util/expand/ViewExtensions.kt
+++ JHelper/src/main/java/top/jessi/jhelper/util/expand/ViewExtensions.kt
@@ -1,6 +1,7 @@
 package top.jessi.jhelper.util.expand
 
 import android.view.View
+import android.view.ViewGroup
 
 object ViewExtensions {
     /**
@@ -38,5 +39,60 @@ object ViewExtensions {
                 onClick(it)
             }
         }
+    }
+
+    /**
+     * 设置控件宽度
+     * @param width 宽度值（px）
+     */
+    fun View.setWidth(width: Int) {
+        val params = layoutParams
+        params.width = width
+        layoutParams = params
+    }
+
+    /**
+     * 设置控件高度
+     * @param height 高度值（px）
+     */
+    fun View.setHeight(height: Int) {
+        val params = layoutParams
+        params.height = height
+        layoutParams = params
+    }
+
+    /**
+     * 同时设置控件宽高
+     * @param width 宽度值（px）
+     * @param height 高度值（px）
+     */
+    fun View.setSize(width: Int, height: Int) {
+        val params = layoutParams
+        params.width = width
+        params.height = height
+        layoutParams = params
+    }
+
+    /**
+     * 设置控件外边距（px）
+     * @param left 左外边距
+     * @param top 上外边距
+     * @param right 右外边距
+     * @param bottom 下外边距
+     */
+    fun View.setMargin(left: Int, top: Int, right: Int, bottom: Int) {
+        if (layoutParams is ViewGroup.MarginLayoutParams) {
+            val params = layoutParams as ViewGroup.MarginLayoutParams
+            params.setMargins(left, top, right, bottom)
+            layoutParams = params
+        }
+    }
+
+    /**
+     * 设置控件四周外边距相同（px）
+     * @param margin 外边距值
+     */
+    fun View.setMargin(margin: Int) {
+        setMargin(margin, margin, margin, margin)
     }
 }
